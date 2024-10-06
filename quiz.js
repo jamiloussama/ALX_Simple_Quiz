d716bcc310b3a3f66504a13872cebffd4d929251
Oussama JAMIL <oussamajamil321@gmail.com>
2024-10-06 13:39:21 +0100

    js function added

---
+++ quiz.js
@@ -0,0 +1,16 @@
+
+function checkAnswer() {
+    var correctAnswer = "4";
+    var selectedOption = document.querySelector('input[name="quiz"]:checked');
+    var userAnswer = selectedOption.value;
+    var feedbackText = document.getElementById('feedback');
+    if (correctAnswer == userAnswer) {
+        feedbackText.textContent="Correct! Well done.";
+    }
+    else{
+        feedbackText.textContent="That's incorrect. Try again!";
+    }
+}
+
+var submitbtn = document.getElementById('submit-answer');
+submitbtn.addEventListener("click",checkAnswer);
